098da8a802fe12f6d1d6cc080dd54a55d534c98f
Subrat-Soul <subrat.rautaray@soulunileaders.com>
2024-07-25 15:47:03 +0530

    learn basics

---
+++ js/first.js
@@ -0,0 +1,7 @@
+console.log("Welcome to VS code ...now you can play with javascript");
+
+
+function btnClick() {
+    document.getElementById("demo").innerHTML =
+     "Hello Dolly.";
+}
---
+++ js/myScript.js
@@ -0,0 +1,5 @@
+function btnClick2() {
+    document.getElementById("demo12").innerHTML = "How are you?";
+    const button = document.getElementById('printBtn');
+    button.style.display  = 'block';
+}
